49767ad38328e7b57f225a022865d533003ef6a1
Zachary Brown <zacdsbrown1994@gmail.com>
2020-04-12 17:25:29 +1000

    first Commit

---
+++ Escanor/escanorsite/escanor1/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Escanor1Config(AppConfig):
+    name = 'escanor1'
---
+++ Escanor/escsetup.py
@@ -0,0 +1,4 @@
+from distils.core import setup
+import py2exe
+
+setup(console=['Escanor.py'])
