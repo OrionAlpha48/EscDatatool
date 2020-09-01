9527769daad0449d4962225aa60fbda5659769e1
Zachary Brown <zacdsbrown1994@gmail.com>
2020-09-01 19:31:01 +1000

    Removal of key

--- Escanor/escanorsite/escanorsite/settings.py
+++ Escanor/escanorsite/escanorsite/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qsnc%i7gfnp4575ddx5&qk#wh7@fs!%-&f_6k1o4)-s0e$(y7+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
